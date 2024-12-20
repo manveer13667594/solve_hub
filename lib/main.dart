969ce79e7eb523c1364aefb5ebec8aa8d1cb2742
Manveer <manveer13667594@gmail.com>
2024-12-20 20:51:40 +0530

    Made folder structure for clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'CalculateScreen.dart';
-import 'Home.dart';
-import 'Profile.dart';
-import 'Search.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -16,71 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Solve Hub',
-      home: const MyHomePage(title: 'Solve Hub'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _children = [
-    HomeScreen(),
-    CalculateScreen(),
-    SearchScreen(),
-    ProfileScreen(),
-  ];
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text(widget.title),
-      ),
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.green, // Set selected item color
-        unselectedItemColor: Colors.grey, // Set unselected item color
-        backgroundColor: Colors.white, // Set background color
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calculate),
-            label: 'Calculate',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Solve Hub'),
+        ),
+        body: const Text('this is body of scaffold'),
       ),
     );
   }
 }
-
-
